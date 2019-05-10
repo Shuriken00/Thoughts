b52ff7a7115472f4a635d8990653f4b5f6838151
Qomplainerz <messanyrecordings@yahoo.de>
2019-05-10 22:37:43 +0200

    Update thoughtsdb.sql

--- thoughtsdb.sql
+++ thoughtsdb.sql
@@ -10,21 +10,14 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
 -- Database: `thoughtsdb`
---
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
---
 
 CREATE TABLE IF NOT EXISTS `users` 
 (
@@ -34,9 +27,7 @@ CREATE TABLE IF NOT EXISTS `users`
   `pass` varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
---
 -- Dumping data for table `users`
---
 
 INSERT INTO `users` 
 (
@@ -65,23 +56,17 @@ VALUES
     '$2y$10$VMnOl2qejf41uBI3uOUsdOWq9bRYxCLDWToIvJqCQt7OKAlTKG2Sq'
 );
 
---
 -- Indexes for dumped tables
---
 
---
 -- Indexes for table `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
 -- AUTO_INCREMENT for dumped tables
---
 
---
 -- AUTO_INCREMENT for table `users`
---
+
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
